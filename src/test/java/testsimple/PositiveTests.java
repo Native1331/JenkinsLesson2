ced2b3b683088488841565d406bf9507899d1194
Natalya.Kadysheva <Native1331>
2022-04-28 17:42:25 +0300

    Add test

---
+++ src/test/java/testsimple/PositiveTests.java
@@ -0,0 +1,31 @@
+package testsimple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+    @Test
+    void test00() {
+        assertTrue(true);
+    }
+    void test01() {
+        assertTrue(true);
+    }
+    void test02() {
+        assertTrue(true);
+    }
+    void test03() {
+        assertTrue(true);
+    } void test04() {
+        assertTrue(true);
+    }
+    void test05() {
+        assertTrue(true);
+    }
+        void test06() {
+            assertTrue(true);
+        }
+    }
+
+
