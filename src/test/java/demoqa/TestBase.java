ec1b0c4337f19c13fd353f5ddc54c6f84d3947c1
Natalya.Kadysheva <Native1331>
2022-05-02 09:51:10 +0300

    Add demoqatest

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -8,23 +8,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static java.lang.String.format;
+
 
 public class TestBase {
-    String firstname="Alex",
-            lastname ="Egorov",
-            email = "alex@erorov.com";
 
-    String expectedFullname = format("%s %s",firstname,lastname);
 
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1680x1050";
+        Configuration.browserSize = "1920x1080x24";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
