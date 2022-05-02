5605604cb10100f0ec9da56c75ce55ace199419c
Natalya.Kadysheva <Native1331>
2022-05-02 18:15:53 +0300

    Add Homework

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -1,5 +1,6 @@
 package helpers;
 
+
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
@@ -12,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
+
 
 public class Attach {
     @Attachment(value = "{attachName}", type = "text/plain")
@@ -36,7 +38,7 @@ public class Attach {
         );
     }
 
-  /**  @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
                 + getVideoUrl(getSessionId())
@@ -53,7 +55,7 @@ public class Attach {
         }
         return null;
     }
-**/
+
     public static String getSessionId(){
         return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
